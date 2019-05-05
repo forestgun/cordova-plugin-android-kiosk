72c768471083380e7e57fcebce179b41fb6b18f5
Jose Fortes <fortes.movil@gmail.com>
2019-05-05 13:10:46 +0200

    Update Kiosk.js
    return Promise

--- www/Kiosk.js
+++ www/Kiosk.js
@@ -1,17 +1,37 @@
-var exec = require('cordova/exec');
+// var exec = require('cordova/exec');
 
-exports.lockLauncher = function(enabled, success, error) {
-    exec(null, error, 'Kiosk', 'lockLauncher', [!!enabled]);
-};
+// exports.lockLauncher = function(enabled, success, error) {
+//     exec(null, error, 'Kiosk', 'lockLauncher', [!!enabled]);
+// };
 
-exports.isLocked = function(arg0, success, error) {
-    exec(success, null, 'Kiosk', 'isLocked', null);
-};
+// exports.isLocked = function(arg0, success, error) {
+//     exec(success, null, 'Kiosk', 'isLocked', null);
+// };
 
-exports.switchLauncher = function() {
-    exec(null, null, 'Kiosk', 'switchLauncher', null);
-};
+// exports.switchLauncher = function() {
+//     exec(null, null, 'Kiosk', 'switchLauncher', null);
+// };
 
-exports.deleteDeviceAdmin = function() {
-    exec(null, null, 'Kiosk', 'deleteDeviceAdmin', null);
-};
+// exports.deleteDeviceAdmin = function() {
+//     exec(null, null, 'Kiosk', 'deleteDeviceAdmin', null);
+// };
+
+cordova.define("cordova-plugin-android-kiosk.Kiosk", function(require, exports, module) {
+
+    module.exports = {
+        lockLauncher: function (enabled, success, error) {
+            return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'Kiosk', 'lockLauncher', [!!enabled]));
+        },
+        isLocked: function (arg0, success, error) {
+            return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'Kiosk', 'isLocked', null));
+        },
+        switchLauncher: function () {
+            return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'Kiosk', 'switchLauncher', null));
+        },
+        deleteDeviceAdmin: function () {
+            return new Promise((resolve, reject) => cordova.exec(resolve, reject, 'Kiosk', 'deleteDeviceAdmin', null));
+        },
+
+    }
+
+    });
