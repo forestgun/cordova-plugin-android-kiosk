6e6f963bf4f4410b2a305388dd8a845711e0a44a
Jose Fortes <fortes.movil@gmail.com>
2019-05-05 13:12:14 +0200

    Update Kiosk.java
    logs development

--- src/android/Kiosk.java
+++ src/android/Kiosk.java
@@ -29,20 +29,26 @@ public class Kiosk extends CordovaPlugin {
     private boolean mIsKioskEnabled;
     private CallbackContext mCallbackContext;
 
+    private static final String TAG = "Kiosk.java";
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         mCallbackContext = callbackContext;
         if (action.equals("lockLauncher")) {
+            Log.d(TAG, "Kiosk.java action = lockLauncher ");
             Boolean locked = args.getBoolean(0);
             lockLauncher(locked);
             return true;
         } else if (action.equals("isLocked")) {
+            Log.d(TAG, "Kiosk.java action = isLocked ");
             callbackContext.success(String.valueOf(mIsKioskEnabled));
             return true;
         } else if (action.equals("switchLauncher")) {
+            Log.d(TAG, "Kiosk.java action = switchLauncher ");
             switchLauncher(callbackContext);
             return true;
         } else if (action.equals("deleteDeviceAdmin")) {
+            Log.d(TAG, "Kiosk.java action = deleteDeviceAdmin ");
             deleteDeviceAdmin();
         }
         return false;
@@ -56,12 +62,14 @@ public class Kiosk extends CordovaPlugin {
 
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        Log.d(TAG, "Kiosk.java initialize() ");
         super.initialize(cordova, webView);
         lockLauncher(true);
         enterFullscreen();
     }
 
     private void enterFullscreen() {
+        Log.d(TAG, "Kiosk.java enterFullscreen() ");
         cordova.getActivity().runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -71,6 +79,7 @@ public class Kiosk extends CordovaPlugin {
     }
 
     private void hideSystemUI() {
+        Log.d(TAG, "Kiosk.java hideSystemUI() ");
         View mDecorView = cordova.getActivity().getWindow().getDecorView();
         // Set the IMMERSIVE flag.
         // Set the content to appear under the system bars so that the content
@@ -98,7 +107,9 @@ public class Kiosk extends CordovaPlugin {
     }
 
     private void switchLauncher(CallbackContext callbackContext) {
+        Log.d(TAG, "Kiosk.java switchLauncher() ");
         if (mIsKioskEnabled) {
+            Log.d(TAG,"Kiosk.java switchLauncher() if(mIsKioskEnabled) true ");
             lockLauncher(false);
         }
         Intent intent = new Intent(Intent.ACTION_MAIN);
@@ -113,56 +124,76 @@ public class Kiosk extends CordovaPlugin {
     }
 
     private void lockLauncher(boolean locked) {
+        Log.d(TAG, "Kiosk.java lockLauncher() ");
         if (locked && !mIsKioskEnabled) {
+            Log.d(TAG, "Kiosk.java lockLauncher() if (locked && !mIsKioskEnabled) true ");
+
             mDpm = (DevicePolicyManager) cordova.getActivity().getSystemService(Context.DEVICE_POLICY_SERVICE);
             ComponentName deviceAdmin = new ComponentName(cordova.getActivity(), MyAdmin.class);
             if (!mDpm.isAdminActive(deviceAdmin)) {
+                Log.d(TAG, "Kiosk.java lockLauncher() not admin active ");
                 callbackMessage(false, "not admin active");
             }
             if (mDpm.isDeviceOwnerApp(cordova.getActivity().getPackageName())) {
+                Log.d(TAG, "Kiosk.java lockLauncher() isDeviceOwnerApp true  ");
                 mDpm.setLockTaskPackages(deviceAdmin, new String[]{cordova.getActivity().getPackageName()});
             } else {
+                Log.d(TAG, "Kiosk.java lockLauncher() not device owner app  ");
                 callbackMessage(false, "not device owner app");
             }
+            Log.d(TAG, "Kiosk.java lockLauncher() enableKioskMode(true) ");
             enableKioskMode(true);
         } else if (!locked) {
+            Log.d(TAG, "Kiosk.java lockLauncher() else if (!locked) true  => enableKioskMode(false) ");
             enableKioskMode(false);
         }
     }
 
     private void enableKioskMode(boolean enabled) {
+        Log.d(TAG, "Kiosk.java enableKioskMode() ");
         try {
             if (enabled) {
+                Log.d(TAG, "Kiosk.java enableKioskMode() enabled true ");
                 if (mDpm.isLockTaskPermitted(cordova.getActivity().getPackageName())) {
+                    Log.d(TAG, "Kiosk.java enableKioskMode() isLockTaskPermitted true ");
                     cordova.getActivity().startLockTask();
                     mIsKioskEnabled = true;
                 } else {
+                    Log.d(TAG, "Kiosk.java enableKioskMode() no permission to lock ");
+                    mIsKioskEnabled = false; // esto es una prueba para que no sea null
                     callbackMessage(false, "no permission to lock");
                 }
             } else {
+                Log.d(TAG, "Kiosk.java enableKioskMode() enabled false ");
                 cordova.getActivity().stopLockTask();
                 mIsKioskEnabled = false;
             }
         } catch (Exception e) {
+            Log.d(TAG, "Kiosk.java enableKioskMode() android lock task exception ");
             callbackMessage(false, "android lock task exception");
             e.printStackTrace();
         }
     }
 
     private void deleteDeviceAdmin() {
+        Log.d(TAG, "Kiosk.java deleteDeviceAdmin() ");
         mDpm.clearDeviceOwnerApp(cordova.getActivity().getPackageName());
     }
 
 
     private void callbackMessage(boolean success, String message) {
+        Log.d(TAG, "Kiosk.java callbackMessage() ");
         if (mCallbackContext == null) {
+            Log.d(TAG, "Kiosk.java callbackMessage() mCallbackContext == null  true");
             return;
         }
         if (success) {
+            Log.d(TAG, "Kiosk.java callbackMessage() success  true");
             PluginResult dataResult = new PluginResult(PluginResult.Status.OK, message);
             dataResult.setKeepCallback(true);
             mCallbackContext.sendPluginResult(dataResult);
         } else {
+            Log.d(TAG, "Kiosk.java callbackMessage() success  false ");
             PluginResult dataResult = new PluginResult(PluginResult.Status.ERROR, message);
             dataResult.setKeepCallback(true);
             mCallbackContext.sendPluginResult(dataResult);
